7c22de0f404aa488ad25c70d21351059d1749626
vidicatering <marketing@ciptasarinavidi.com>
2024-07-18 12:56:20 +0700

    update

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,4 +1,5 @@
 // src/app/wedding-catering.tsx
+"use client";
 
 import React from "react";
 import Nav from "../component/Nav";
@@ -14,6 +15,28 @@ import Gallery from "../component/Gallery";
 import Contact from "../component/Contact";
 import Footer from "../component/Footer";
 import aboutus from "/public/img/about_us.webp";
+import "react-alice-carousel/lib/alice-carousel.css";
+import logo from "/public/img/iso_1.png";
+import dynamic from "next/dynamic";
+
+const AliceCarousel = dynamic(() => import("react-alice-carousel"), { ssr: false });
+
+const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => e.preventDefault();
+
+const items = [
+  { id: 1, src: logo, alt: "Client 1" },
+  { id: 2, src: logo, alt: "Client 2" },
+  { id: 3, src: logo, alt: "Client 3" },
+  { id: 4, src: logo, alt: "Client 4" },
+  { id: 5, src: logo, alt: "Client 5" },
+  { id: 6, src: logo, alt: "Client 6" },
+  { id: 7, src: logo, alt: "Client 7" },
+  { id: 8, src: logo, alt: "Client 8" },
+].map((item) => (
+  <div key={item.id} className="flex justify-center" onDragStart={handleDragStart} role="presentation">
+    <Image src={item.src} alt={item.alt} className="h-24 w-auto" />
+  </div>
+));
 
 const about_us = () => {
   return (
@@ -51,71 +74,105 @@ const about_us = () => {
             <p className="font-argent text-lg">Menyajikan makanan dan pelayanan yang berkualitas dengan keamanan pangan yang terjamin dan harga terjangkau agar konsumen mendapatkan pengalaman terbaik di momen berharga.</p>
           </div>
         </div>
-        <div className="min-h-max bg-black justify-center items-center">
-          <div className="flex flex-col text-center justify-center h-max w-full bg-white shadow-2xl rounded-xl mx-20 py-10">
-            <h1 className="text-brown text-4xl font-argent mb-4">VISI</h1>
-          </div>
-        </div>
-      </div>
-      <div className="min-h-max bg-white w-full flex justify-center items-center pb-32 pt-10">
-        <div className="min-h-max grid grid-cols-1 justify-center items-center gap-10">
-          <div className="flex flex-col text-center justify-center h-max bg-white shadow-2xl rounded-xl mx-36 py-10">
-            <h1 className="text-brown text-4xl font-argent mb-4">VISI</h1>
-            <p className="font-argent text-lg">
-              Menjadikan VIDI Group sebagai perusahaan jasa boga terbaik di Indonesia dimana semua orang bisa mendapatkan jasa catering, pelayanan, dan keamanan pangan yang terbaik dengan harga terjangkau.
+        <div className="min-h-max flex items-center justify-center mb-0 mt-14 mx-32">
+          <div className=" p-6 rounded-tl-[80px] rounded-tr-xl rounded-bl-xl rounded-br-[80px] w-[1336px] h-max flex flex-col items-center py-10 px-8">
+            <p className="font-argent text-3xl text-center">
+              Lebih dari 35 tahun pengalaman, VIDI Group telah dipercaya lebih dari 300 institusi terkemuka, melayani 5000<span className="font-mukta">+</span> Wedding<span className="font-serif"> & </span>Event dengan 1000 lebih menu
+              pilihan. Kami mendedikasikan pengalaman dalam bidang ini untuk memberikan pelayanan terbaik di moment berharga anda
             </p>
           </div>
         </div>
       </div>
-
-      <div className=" flex items-center justify-center ">
-        <div className="bg-white p-6 rounded-lg max-w-7xl w-full mx-32">
-          <h2 className="text-5xl font-bold text-yellow-600 text-center mb-4">Our Offering</h2>
-          <p className="text-center text-gray-600 mb-8">Lorem ipsum dolor sit amet consectetur. Metus sed fringilla.</p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14">
-            <div className="flex flex-col items-center bg-white rounded-b-3xl rounded-t-[150px] shadow-md p-4 ">
-              <Image src={Wedding} alt="Stall" className="rounded-3xl mb-4 hover:shadow-xl hover:scale-105 duration-700" />
-              <h3 className="text-xl font-medium">Wedding</h3>
+      <div className="flex flex-col items-center justify-center mb-20 mx-32 h-28">
+        <div className="bg-white p-6 rounded-lg max-w-7xl w-full">
+          <AliceCarousel
+            mouseTracking
+            items={items}
+            responsive={{
+              0: { items: 1 },
+              568: { items: 2 },
+              1024: { items: 4 },
+            }}
+            autoPlay
+            autoPlayInterval={3000}
+            disableButtonsControls={true}
+            infinite
+          />
+        </div>
+      </div>
+      <div className="min-h-screen flex items-center justify-center my-0 mb-12">
+        <div className="bg-white p-6 rounded-3xl border border-bg_vidi max-w-[1336px] min-h-max shadow-xl">
+          <div className="grid grid-cols-2 items-center px-10 py-14 gap-8 text-3xl text-vidi font-medium font-mukta">
+            <div className="grid grid-cols-1 gap-y-7">
+              <div className="bg-white rounded-xl flex  max-h-max items-center px-8 py-4 min-w-96 ">
+                <div className="">
+                  <h1 className="mb-2">2011 - Kemenag DKI Jakarta</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam menyediakan catering jamaah haji DKI Jakarta di Mekkah, Arab Saudi.</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-xl flex  max-h-max items-center px-8 py-4 min-w-96 ">
+                <div className="">
+                  <h1 className="mb-2">2015 - Badan Narkotika Nasional</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam pengadaan makanan di Balai Besar Rehabilitasi BNN</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-xl flex  min-w-96 max-h-max items-center px-8 py-4 ">
+                <div className="">
+                  <h1 className="mb-2">2015 - Lembaga Sandi Negara</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam pengadaan konsumsi mahasiswa Sekolah Tinggi Sandi Negara</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-xl  flex min-w-96 max-h-max items-center px-8 py-4">
+                <div className="">
+                  <h1 className="mb-2">2016 - Kementerian Keuangan</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam pengadaan konsumsi Diklat Tahap I Pusdiklat Bea Dan Cukai Tahun 2016.</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-xl flex  max-h-max items-center px-8 py-4 min-w-96">
+                <div className="">
+                  <h1 className="mb-2">2017 - Politeknik Ilmu Pelayaran</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam pengadaan konsumsi Diklat Keterampilan Khusus Pelaut di Semarang.</p>
+                </div>
+              </div>
             </div>
-            <div className="flex flex-col items-center bg-white rounded-b-3xl rounded-t-[150px] shadow-md p-4  ">
-              <Image src={Meeting} alt="Buffet" className="rounded-3xl mb-4 hover:shadow-xl hover:scale-105 duration-700 " />
-              <h3 className="text-xl font-medium">Meeting</h3>
-            </div>
-            <div className="flex flex-col items-center bg-white rounded-b-3xl rounded-t-[150px] shadow-md p-4">
-              <Image src={Gathering} alt="Nasi Kotak" className="rounded-3xl mb-4 hover:shadow-xl hover:scale-105 duration-700" />
-              <h3 className="text-xl font-medium">Gathering</h3>
-            </div>
-            <div className="flex flex-col items-center bg-white rounded-b-3xl rounded-t-[150px] shadow-md p-4 ">
-              <Image src={Syukuran} alt="Stall" className="rounded-3xl mb-4 hover:shadow-xl hover:scale-105 duration-700" />
-              <h3 className="text-xl font-medium">Syukuran</h3>
-            </div>
-            <div className="flex flex-col items-center bg-white rounded-b-3xl rounded-t-[150px] shadow-md p-4  ">
-              <Image src={Wisuda} alt="Buffet" className="rounded-3xl mb-4 hover:shadow-xl hover:scale-105 duration-700 " />
-              <h3 className="text-xl font-medium">Wisuda</h3>
-            </div>
-            <div className="flex flex-col items-center bg-white rounded-b-3xl rounded-t-[150px] shadow-md p-4">
-              <Image src={Ballroom} alt="Nasi Kotak" className="rounded-3xl mb-4 hover:shadow-xl hover:scale-105 duration-700" />
-              <h3 className="text-xl font-medium">Ballroom Rental</h3>
+            <div className="grid grid-cols-1 gap-y-7">
+              <div className="bg-white rounded-xl flex  min-w-96 max-h-max items-center px-8 py-4 ">
+                <div className="">
+                  <h1 className="mb-2">2018 - Kemendikbudristek</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam pengadaan konsumsi Seminar Pendidikan Matematika.</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-xl  flex min-w-96 max-h-max items-center px-8 py-4">
+                <div className="">
+                  <h1 className="mb-2">2020 - Akademi Imigrasi</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam pengadaan konsumsi makan Taruna di Politeknik Imigrasi BPSDM Hukum dan HAM</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-xl flex  max-h-max items-center px-8 py-4 min-w-96">
+                <div className="">
+                  <h1 className="mb-2">2022 - PKN STAN</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam pengadaan konsumsi mahasiswa Politeknik Keuangan Negara STAN</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-xl  flex min-w-96 max-h-max items-center px-8 py-4">
+                <div className="">
+                  <h1 className="mb-2">2023 - Embarkasi Haji Bekasi</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam menyediakan catering jamaah haji Bekasi</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-xl  flex min-w-96 max-h-max items-center px-8 py-4">
+                <div className="">
+                  <h1 className="mb-2">2023 - Embarkasi Haji Indramayu</h1>
+                  <p className="text-xl text-brown font-argent">VIDI berkontribusi dalam pengadaan makanan di Balai Besar Rehabilitasi BNN</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="min-h-screen flex items-center justify-center my-0 mx-32">
-        <div className="bg-bg_vidi p-6 rounded-tl-[80px] rounded-tr-xl rounded-bl-xl rounded-br-[80px] max-w-[1336px] max-h-[624px]">
-          <div className="flex flex-col md:flex-row items-center px-20 py-14 gap-10">
-            <div className="md:w-1/2 relative rounded-tl-[50px] rounded-bl-xl rounded-tr-xl rounded-br-[50px] max-w-[445px] max-h-[372px] overflow-hidden">
-              <Image src={Ballroom} alt="Grha Sarina Vidi" className="" />
-            </div>
-            <div className="md:w-1/2 md:pl-6 mt-4 md:mt-0">
-              <h3 className="text-2xl font-semibold text-vidi mb-2">Ballroom & Convention Hall</h3>
-              <div className="w-16 border-b-2 border-vidi mb-4"></div>
-              <h2 className="text-4xl font-bold mb-4">Grha Sarina Vidi</h2>
-              <p className="mb-4 text-justify">
-                Berdiri sejak tahun 2003 Grha Sarina Vidi mampu menampung kapasitas tamu undangan sebanyak 1500 di setiap acara. Dengan fasilitas yang mendukung yang di sediakan GSV dan dibantu Vidi Catering yang sudah berpengalaman lebih
-                dari 38 tahun Sehingga mampu menyajikan Fasilitas dan jasa boga secara profesional yang membuat pesta pernikahhan ataupun (MICT) Meeting, Incentive, Convention, & Exhibition terasa mewah.
-              </p>
-            </div>
-          </div>
+      <div className="min-h-max flex items-center justify-center mt-14 mb-20 mx-32">
+        <div className=" p-6 rounded-tl-[80px] rounded-tr-xl rounded-bl-xl rounded-br-[80px] w-[1336px] h-max flex flex-col items-center py-10 px-8">
+          <p className="font-argent text-3xl text-center">Mendapatkan pelayanan catering terbaik dengan harga terjangkau adalah hak setiap orang.</p>
         </div>
       </div>
       <Contact />
