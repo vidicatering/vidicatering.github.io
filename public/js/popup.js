8f9a260c47bf7a53dc75e371edae183f85595671
vidicatering <marketing@ciptasarinavidi.com>
2024-07-29 15:21:08 +0700

    update popup

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,11 +1,15 @@
 const popup = document.getElementById("popup");
 const dismiss = document.getElementById("popup_close");
 
-setTimeout(function () {
-  popup.classList.add("popup_show");
-}, 2000);
+if (popup && dismiss) {
+  setTimeout(function () {
+    popup.classList.add("popup_show");
+  }, 2000);
 
-dismiss.addEventListener("click", function () {
-  popup.classList.remove("popup_show");
-  popup.classList.add("popup_close");
-});
+  dismiss.addEventListener("click", function () {
+    popup.classList.remove("popup_show");
+    popup.classList.add("opacity-0");
+  });
+} else {
+  console.error("Popup or dismiss button not found in the DOM.");
+}
--- src/app/component/Popup.tsx
+++ src/app/component/Popup.tsx
@@ -3,19 +3,22 @@ import Script from "next/script";
 import React, { useEffect } from "react";
 import Image from "next/image";
 import kabah from "/public/img/kaabah.webp";
-import senja from "/public/img/senja.webp";
-import umrah from "/public/img/umrah.webp";
 
 const PopUp = () => {
   return (
     <>
-      <Script src="/js/popup.js" />
-      <div id="popup" className="opacity-0 transition-all duration-1000 ease-in w-full h-full fixed bg-white bg-opacity-80 m-0 p-0">
-        <div className="  mx-auto ">
-          {/* <div className="max-w-xs lg:max-w-7xl md:max-w-2xl flex items-center justify-center m-auto ">
-            <div className="overflow-hidden rounded-xl shadow-2xl md:grid md:grid-cols-3 bg-white">
+      <Script src="/js/popup.js" />;
+      <div id="popup" className="opacity-0 transition-all duration-1000 ease-in w-full h-full fixed bg-white bg-opacity-80 ">
+        <div className="  mx-auto mt-60">
+          <div className=" h-10">
+            <button id="popup_close" className="absolute right-40 top-40 font-mukta font-bold text-lg">
+              X
+            </button>
+          </div>
+          <div className="max-w-xs lg:max-w-7xl flex items-center justify-center m-auto ">
+            <div className="overflow-hidden rounded-lg shadow-2xl md:grid md:grid-cols-3 bg-white">
               <Image src={kabah} alt="kabah" className="h-60 w-full object-cover md:h-full " />
-              <div className="p-4 text-center sm:p-6 md:col-span-2 lg:p-8 m-auto">
+              <div className="p-4 text-center sm:p-6 md:col-span-2 lg:p-8">
                 <p className="text-sm font-semibold uppercase tracking-widest">🎉 Promo Spesial! 🎉</p>
 
                 <h2 className="mt-6 font-black uppercase">
@@ -31,67 +34,6 @@ const PopUp = () => {
                 <p className="mt-8 text-xs font-medium uppercase text-gray-400">Syarat dan Ketentuan Berlaku *</p>
               </div>
             </div>
-          </div> */}
-          <div className="max-w-max m-auto">
-            <div className=" h-10">
-              <button id="popup_close" className="absolute top-14 right-6 lg:right-[700px] font-mukta font-bold text-lg">
-                X
-              </button>
-            </div>
-            <Image src={umrah} alt="senja wedding" className="m-auto max-h-screen w-auto pb-20 pt-10 " />
-            {/*
-  Heads up! 👋
-
-  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
-*/}
-
-            {/* Base - Right */}
-
-            <a className="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500" href="#">
-              <span className="absolute -start-full transition-all group-hover:start-4">
-                <svg className="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                </svg>
-              </span>
-
-              <span className="text-sm font-medium transition-all group-hover:ms-4"> Download </span>
-            </a>
-
-            {/* Border - Right */}
-
-            <a className="group relative inline-flex items-center overflow-hidden rounded border border-current px-8 py-3 text-indigo-600 focus:outline-none focus:ring active:text-indigo-500" href="#">
-              <span className="absolute -start-full transition-all group-hover:start-4">
-                <svg className="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                </svg>
-              </span>
-
-              <span className="text-sm font-medium transition-all group-hover:ms-4"> Download </span>
-            </a>
-
-            {/* Base - Left */}
-
-            <a className="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500" href="#">
-              <span className="absolute -end-full transition-all group-hover:end-4">
-                <svg className="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                </svg>
-              </span>
-
-              <span className="text-sm font-medium transition-all group-hover:me-4"> Download </span>
-            </a>
-
-            {/* Border - Left */}
-
-            <a className="group relative inline-flex items-center overflow-hidden rounded border border-current px-8 py-3 text-indigo-600 focus:outline-none focus:ring active:text-indigo-500" href="#">
-              <span className="absolute -end-full transition-all group-hover:end-4">
-                <svg className="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                </svg>
-              </span>
-
-              <span className="text-sm font-medium transition-all group-hover:me-4"> Download </span>
-            </a>
           </div>
         </div>
       </div>
