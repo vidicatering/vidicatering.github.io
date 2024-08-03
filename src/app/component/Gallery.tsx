74598728a7ede23a0caa385b42dfd9a225d73795
vidicatering <marketing@ciptasarinavidi.com>
2024-08-03 10:52:50 +0700

    update

--- src/app/component/Gallery.tsx
+++ src/app/component/Gallery.tsx
@@ -3,11 +3,11 @@ import Image, { ImageProps } from "next/image";
 import Stall from "/public/img/Stall.webp";
 import Buffet from "/public/img/Buffet.webp";
 import NasiKotak from "/public/img/nasi_box.webp";
-import TumpengMini from "/public/img/tumpeng_mini.webp";
-import gsv from "/public/img/gsv.webp";
+import Image12 from "/public/img/image10.webp";
+import Penglaris from "/public/img/image12.webp";
 import Wedding from "/public/img/Wedding.webp";
 import Gathering from "/public/img/Gathering.webp";
-import Ballroom from "/public/img/ballroom.webp";
+import Image11 from "/public/img/image11.webp";
 import Meeting from "/public/img/Meeting.webp";
 import Wisuda from "/public/img/Wisuda.webp";
 import Syukuran from "/public/img/Syukuran.webp";
@@ -22,12 +22,12 @@ const images: CustomImageProps[] = [
   { src: Stall, alt: "Stall" },
   { src: Buffet, alt: "Buffet" },
   { src: NasiKotak, alt: "Nasi Kotak" },
-  { src: TumpengMini, alt: "Tumpeng Mini" },
+  { src: Image11, alt: "Tumpeng Mini" },
   { src: hampers, alt: "Hampers" },
-  { src: gsv, alt: "Grha Sarina Vidi" },
+  { src: Penglaris, alt: "Penglaris Heri T" },
   { src: Wedding, alt: "Wedding" },
   { src: Gathering, alt: "Gathering" },
-  { src: Ballroom, alt: "Ballroom" },
+  { src: Image12, alt: "Ballroom" },
   { src: Meeting, alt: "Meeting" },
   { src: Wisuda, alt: "Wisuda" },
   { src: Syukuran, alt: "Syukuran" },
@@ -50,10 +50,10 @@ const Gallery: React.FC = () => {
         <div className="mt-16 mb-12">
           <h1 className="text-5xl font-bold text-yellow-600 text-center">Gallery</h1>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mx-10 lg:mx-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mx-10 lg:mx-2 ">
           {shuffledImages.map((image, index) => (
             <div key={index} className="">
-              <Image src={image.src} alt={image.alt} className="rounded-lg" />
+              <Image src={image.src} alt={image.alt} className="rounded-lg " />
             </div>
           ))}
         </div>
--- src/app/component/Popup.tsx
+++ src/app/component/Popup.tsx
@@ -2,35 +2,37 @@
 import Script from "next/script";
 import React, { useEffect } from "react";
 import Image from "next/image";
-import kabah from "/public/img/kaabah.webp";
+import image11 from "/public/img/Buffet.webp";
+import Link from "next/link";
 
 const PopUp = () => {
   return (
     <>
       <Script src="/js/popup.js" />;
       <div id="popup" className="opacity-0 transition-all duration-1000 ease-in w-full h-full fixed bg-white bg-opacity-80 ">
-        <div className="  mx-auto mt-60">
+        <div className="  mx-auto lg:mt-60 mt-10">
           <div className=" h-10">
-            <button id="popup_close" className="absolute right-40 top-40 font-mukta font-bold text-lg">
+            <button id="popup_close" className="absolute right-10 top-10 lg:right-72 lg:top-60 font-mukta font-black text-xl">
               X
             </button>
           </div>
-          <div className="max-w-xs lg:max-w-7xl flex items-center justify-center m-auto ">
-            <div className="overflow-hidden rounded-lg shadow-2xl md:grid md:grid-cols-3 bg-white">
-              <Image src={kabah} alt="kabah" className="h-60 w-full object-cover md:h-full " />
-              <div className="p-4 text-center sm:p-6 md:col-span-2 lg:p-8">
-                <p className="text-sm font-semibold uppercase tracking-widest">🎉 Promo Spesial! 🎉</p>
-
-                <h2 className="mt-6 font-black uppercase">
-                  <span className="text-4xl font-black sm:text-5xl lg:text-6xl"> GRATIS Tiket Umroh!</span>
-
-                  <span className="mt-2 px-4 block text-sm">& All You Can Eat Food Testing at Grha Sarina Vidi</span>
-                </h2>
-
-                <a className="mt-4 inline-block w-full bg-vidi rounded-lg py-4 text-sm font-bold uppercase tracking-widest text-white" href="#">
-                  KLAIM SEKARANG!
-                </a>
-
+          <div className="max-w-xs md:max-w-4xl lg:max-w-7xl flex items-center justify-center m-auto ">
+            <div className="overflow-hidden rounded-lg shadow-2xl md:grid md:grid-cols-3 bg-white ">
+              <Image src={image11} alt="kabah" className="h-60 w-full object-cover md:h-full " />
+              <div className="p-4 text-center sm:p-6 md:col-span-2 lg:p-8 m-auto">
+                <h2 className="mt-2 px-4 block lg:text-xl text-base font-bold uppercase"> All You Can Eat Food Testing at Grha Sarina Vidi</h2>
+                <h1 className="mt-2 text-4xl font-black sm:text-5xl lg:text-6xl uppercase">GRATIS Tiket Umroh!</h1>
+                <button className=" mt-6 inline-block w-full bg-vidi rounded-lg py-4 text-sm font-bold uppercase tracking-widest text-white">
+                  <Link
+                    href="https://wa.me/628112546265?text=Hai%20Admin%20VIDI%20Catering%2C%20Saya%20ingin%20bergabung%20di%20acara%20Free%20Food%20Tasting%20Flight%20Batch%202"
+                    passHref
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className=""
+                  >
+                    KLAIM SEKARANG!
+                  </Link>
+                </button>
                 <p className="mt-8 text-xs font-medium uppercase text-gray-400">Syarat dan Ketentuan Berlaku *</p>
               </div>
             </div>
