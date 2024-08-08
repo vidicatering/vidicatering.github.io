cc8f57d1715a050a52b8bfd3e4a81330c11f487c
vidicatering <marketing@ciptasarinavidi.com>
2024-08-08 14:42:45 +0700

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import { prisma } from "./prisma";
 
 export const getBlogs = async () => {
   try {
-    const blogs = await prisma.BlogModel.findMany();
+    const blogs = await prisma.blogModel.findMany();
     return blogs;
   } catch (error) {
     throw new Error("Error fetching blogs");
