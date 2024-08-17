321cef86edebb0a548c98896434d72ca2294a4d7
vidicatering <marketing@ciptasarinavidi.com>
2024-08-17 22:22:38 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,40 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${argent.variable}`}>
-      <body>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !(function(f, b, e, v, n, t, s) {
+                if (f.fbq) return;
+                n = f.fbq = function() {
+                  n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = !0;
+                n.version = "2.0";
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = !0;
+                t.src = v;
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s);
+              })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '418200921271699');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=418200921271699&ev=PageView&noscript=1" />
+        </noscript>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-TC7VE7M22E"></script>
         <script></script>
         <Script src="/js/googleAnalytics.js" />
         <SpeedInsights />
+      </head>
+      <body>
         <main>
           <ClientLayout>{children}</ClientLayout>
         </main>
