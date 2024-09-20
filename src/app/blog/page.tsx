1ff2d80b28d285f032dd63480ba6f68dc9654700
vidicatering <marketing@ciptasarinavidi.com>
2024-09-20 11:01:53 +0700

    update loading

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,25 +2,38 @@
 
 import { useEffect, useState } from "react";
 import { ArticleCardLatest, ArticleCardSmall } from "../components/article-card";
-import { Blog, getBlogs } from "../../../lib/data"; // Pastikan tipe Blog diimport
+import { Blog } from "../../../lib/data";
 import { formatDate } from "../../../lib/utils";
+import Loader from "../components/loading";
 
 const BlogPage = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]); // Tentukan tipe Blog[]
+  const [loading, setLoading] = useState(true); // State untuk loading
 
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
         const res = await fetch("/api/blog", { cache: "no-store" });
-        const blogsData: Blog[] = await res.json(); // Tentukan tipe sebagai Blog[]
+        const blogsData: Blog[] = await res.json();
         setBlogs(blogsData.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()));
       } catch (error) {
         console.error("Error fetching blogs:", error);
+      } finally {
+        setLoading(false); // Setelah fetch selesai, matikan status loading
       }
     };
 
     fetchBlogs();
   }, []);
+
+  // Placeholder untuk Lazy Loading
+  if (loading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <Loader />
+      </div>
+    );
+  }
 
   const latestBlog = blogs[0];
   const recentBlogs = blogs.slice(1, 5);
